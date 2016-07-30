f6a6528cca6d20033b3c70f0b210e62f747972ea
Alex Klibisz <aklibisz@gmail.com>
2016-07-30 11:47:30 -0400

    more thorough comments

--- deep-learning-crash-course/03-keras-intro/keras-pima.py
+++ deep-learning-crash-course/03-keras-intro/keras-pima.py
@@ -9,25 +9,37 @@ ap.add_argument('-w', '--weights', required=False, help='file with weights')
 ap.add_argument('-s', '--seed', required=False, help='random seed')
 args = vars(ap.parse_args())
 
-# Seed numpy
-seed = 7
-numpy.random.seed(seed)
+# Seed numpy, which will affect how the NN weights are initialized.
+if args["seed"] is not None:
+  numpy.random.seed(int(args["seed"]))
+else:
+  numpy.random.seed(7)
 
-# Load the dataset
+# Load the Pima Indians dataset
 print("Loading dataset...")
 dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 features = dataset[:, 0:8] # features are columns 0 through 8 
-labels = dataset[:, 8]   # prediction value is the 9th column
+labels = dataset[:, 8]     # label value is the 9th column
 
 # Define the model
 print("Defining model...")
 model = Sequential()
+# Layer 1: dense layer with 12 outputs, 8 inputs, 
+# uniform weight initialization, rectified linear unit activation 
 model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+
+# Layer 2: dense layer with 8 outputs, uniform weight initialization,
+# rectified linear unit activation
 model.add(Dense(8, init='uniform', activation='relu'))
+
+# Layer 3: dense layer with 1 output, uniform weight initialization,
+# sigmoid activation function
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 
 # Compile the model
 print("Compilng model...")
+# Compiled to minimize binary_crossentropy (logloss) using Adaptive Moment Estimation
+# and measuring accuracy
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Load weights if defined or fit the model
@@ -35,7 +47,8 @@ if args["weights"] is not None:
   print("Loading weights...")
   model.load_weights(args["weights"])
 else:
-  print("Fitting...")
+  print("Fitting and saving weights...")
+  # Fit the model for 150 epochs in batches of 10
   model.fit(features, labels, nb_epoch=150, batch_size=10, verbose=0)
   model.save_weights("out.h5", overwrite=True)
 
